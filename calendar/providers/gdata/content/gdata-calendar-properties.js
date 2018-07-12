1b81bbc40a9149156084b32830ff196b7e80fd52
Philipp Kewisch <mozilla@kewis.ch>
2018-07-12 14:32:06 +0200

    Bug 1475224 - Fix gdata calendar properties dialog overlay. r=MakeMyDay
    --HG--
    extra : rebase_source : f569b7e9e722b6c438b322f87a96d945e43b29ae

--- calendar/providers/gdata/content/gdata-calendar-properties.js
+++ calendar/providers/gdata/content/gdata-calendar-properties.js
@@ -2,32 +2,34 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-(function() {
-    ChromeUtils.import("resource://gdata-provider/modules/gdataUtils.jsm");
-    monkeyPatch(window, "onLoad", function(protofunc, ...args) {
-        let rv = protofunc.apply(this, args);
-        if (gCalendar.type == "gdata") {
-            let accessRole = gCalendar.getProperty("settings.accessRole");
-            let isReader = (accessRole == "freeBusyReader" || accessRole == "reader");
-            let isEventsCalendar = gCalendar.getProperty("capabilities.events.supported");
-            let isDisabled = gCalendar.getProperty("disabled");
+window.addEventListener("load", () => {
+    if (document.getElementById("calendar-uri").value) {
+        // Calendar's load function needs to be called first, and that seems to have happened.
+        gdataOnLoad();
+    } else {
+        // onLoad has not yet been called, so we can piggyback on that function.
+        monkeyPatch(window, "onLoad", function(protofunc, ...args) {
+            let rv = protofunc(...args);
+            gdataOnLoad();
+            return rv;
+        });
+    }
+});
 
-            // Disable setting read-only if the calendar is readonly anyway
-            document.getElementById("read-only").disabled = isDisabled || (isEventsCalendar && isReader);
+function gdataOnLoad() {
+    if (gCalendar.type == "gdata") {
+        let accessRole = gCalendar.getProperty("settings.accessRole");
+        let isReader = (accessRole == "freeBusyReader" || accessRole == "reader");
+        let isEventsCalendar = gCalendar.getProperty("capabilities.events.supported");
+        let isDisabled = gCalendar.getProperty("disabled");
 
-            // Don't allow setting refresh interval to less than 30 minutes
-            let refInterval = document.getElementById("calendar-refreshInterval-menupopup");
-            Array.from(refInterval.childNodes).filter((node) => {
-                let nodeval = parseInt(node.getAttribute("value"), 10);
-                return nodeval < 30 && nodeval != 0;
-            }).forEach((node) => { refInterval.removeChild(node); });
-
-            // Old Lightning doesn't hide the cache label
-            let oldCacheLabel = document.getElementById("cache");
-            if (oldCacheLabel) {
-                oldCacheLabel.setAttribute("hidden", "true");
-            }
-        }
-        return rv;
-    });
-})();
+        // Disable setting read-only if the calendar is readonly anyway
+        document.getElementById("read-only").disabled = isDisabled || (isEventsCalendar && isReader);
+       // Don't allow setting refresh interval to less than 30 minutes
+       let refInterval = document.getElementById("calendar-refreshInterval-menupopup");
+       Array.from(refInterval.childNodes).filter((node) => {
+           let nodeval = parseInt(node.getAttribute("value"), 10);
+           return nodeval < 30 && nodeval != 0;
+       }).forEach((node) => { refInterval.removeChild(node); });
+   }
+}
