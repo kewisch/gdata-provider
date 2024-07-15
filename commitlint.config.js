3c9048cc290c7dbb45040f6e64691bcbf71d0178
Philipp Kewisch <hello@kewis.ch>
2024-07-15 10:56:32 +0200

    chore: Linting for commitlint.config.js on legacy

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
-  ignores: [
-    (message) => message.startsWith("chore(deps")
-  ],
+  ignores: [message => message.startsWith("chore(deps")],
   rules: {
-    "subject-case": [
-      2,
-      "always",
-      ["sentence-case"]
-    ]
-  }
+    "subject-case": [2, "always", ["sentence-case"]],
+  },
 };
