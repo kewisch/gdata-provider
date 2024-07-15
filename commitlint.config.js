8a12b15c857b3f392da5f577fc3bc185b47930b9
Philipp Kewisch <hello@kewis.ch>
2024-07-15 10:54:56 +0200

    chore: Include commitlint config on legacy

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [
+    (message) => message.startsWith("chore(deps")
+  ],
+  rules: {
+    "subject-case": [
+      2,
+      "always",
+      ["sentence-case"]
+    ]
+  }
+};
