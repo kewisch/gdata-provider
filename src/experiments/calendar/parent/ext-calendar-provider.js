01258f6f5baf6ee2d05a2b45822c033a5cad6daa
Philipp Kewisch <mozilla@kewis.ch>
2026-02-28 12:23:40 +0100

    fix: Use readable stream for 148+

--- src/experiments/calendar/parent/ext-calendar-provider.js
+++ src/experiments/calendar/parent/ext-calendar-provider.js
@@ -10,6 +10,8 @@ var { ExtensionSupport } = ChromeUtils.importESModule("resource:///modules/Exten
 
 var { CalItipEmailTransport } = ChromeUtils.importESModule("resource:///modules/CalItipEmailTransport.sys.mjs");
 
+var { CalReadableStreamFactory } = ChromeUtils.importESModule("resource:///modules/CalReadableStreamFactory.sys.mjs");
+
 // TODO move me
 // Have the server take care of scheduling. This can be de-duplicated in
 // CalItipEmailTransport.sys.mjs
@@ -475,7 +477,11 @@ class ExtCalendar extends cal.provider.BaseClass {
   }
 
   getItems(_aFilter, _aCount, _aRangeStart, _aRangeEnd) {
-    return this.offlineStorage.getItems(...arguments);
+    if (!this.mOfflineStorage) {
+      return CalReadableStreamFactory.createEmptyReadableStream();
+    }
+
+    return this.mOfflineStorage.getItems(...arguments);
   }
 
   refresh() {
